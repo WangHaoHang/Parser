29cf4317014a8373c72b841e7a4dc0dce16535d8
Hang <Hang@163.com>
2022-07-07 00:16:53 +0800

    增加word_table结构体

--- token/__init__.py
+++ token/__init__.py
@@ -6,5 +6,23 @@ class state_node(object):
         self.desc = ''
         self.next = None
         
+class word_table(object):
+    '''存放词法分析数据
+
+    :param object: _description_
+    :type object: _type_
+    '''
+    def __init__(self) -> None:
+        
+        self.identifier=['while','for','if','else','int','float','byte','double','str','break','continue'] #标识符
+        self.words=[] # 存放的数据
+    
+    def print(self):
+        '''_summary_
+
+        :return: _description_
+        :rtype: _type_
+        '''
+        print('words:',self.words)
 if __name__ == '__main__':
     print('token')
